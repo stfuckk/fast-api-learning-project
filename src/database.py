2a54e20a43dd8bc7fdcc9f39c8f882e401ce86ec
Kozlov Denis <denkis2205@gmail.com>
2024-08-26 18:29:51 +0700

    init fastapi project

---
+++ src/database.py
@@ -0,0 +1,16 @@
+# Подключение БД
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from dotenv import load_dotenv
+import os
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print(DATABASE_URL)
